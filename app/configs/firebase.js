fc6173301be09e78aa64c3aae4190250c8a09386
Abhishek Kashyap <abhishekkashyap092@gmail.com>
2018-04-04 13:48:32 +0530

    adding the Android
    added the android files and component

---
+++ app/configs/firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBgaMm5mN4VmW-cRomqucvzHXPRX6g7aC8",
+  authDomain: "firstfire-cc0c1.firebaseapp.com",
+  databaseURL: "https://firstfire-cc0c1.firebaseio.com",
+  projectId: "firstfire-cc0c1",
+  storageBucket: "firstfire-cc0c1.appspot.com",
+  messagingSenderId: "1062700606634",
+}
